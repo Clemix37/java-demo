5430a7f1ff22425b2fb1458a9bef18ccbbc59075
H4ckW1s3r <clementix01@gmail.com>
2025-08-31 12:31:24 +0200

    Added comments

--- src/main/java/FindNumber.java
+++ src/main/java/FindNumber.java
@@ -2,18 +2,23 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class FindNumber {
+    // Store the high and the low numbers
     public static final int low = 0;
     public static final int high = 100;
 
-    public static void play(){
+    public static void play() {
+        // Create a secret code between high and low
         int secret = getSecretCode();
         Scanner scanner = new Scanner(System.in);
         int userInput = low-1;
         System.out.println("Find the number between " + low + " and " + high);
+        // While user's guess is not the secret number
         while (userInput != secret) {
+            // Get user's guess
             System.out.println("Your guess : ");
             String userAnswer = scanner.nextLine();
             userInput = Integer.parseInt(userAnswer);
+            // Check for less or greater
             if (userInput > secret){
                 System.out.println("Less");
             }
@@ -21,12 +26,15 @@ public class FindNumber {
                 System.out.println("Greater");
             }
         }
+        // When user's guess is correct, we break out of loop and display a message
         System.out.println("You won !! The secret was indeed " + secret);
         String answer = "";
+        // Ask for replaying
         while(!answer.equals("y") && !answer.equals("n") && !answer.equals("yes") && !answer.equals("no")){
             System.out.println("Wanna play again ? (y/n)");
             answer = scanner.nextLine().toLowerCase();
         }
+        // If yes, play again
         if (answer.equals("y") || answer.equals("yes")) play();
     }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        // Display title and options
         System.out.println("Welcome to CyberWebDev tools, what do you need ?");
         System.out.println("1. Find the number game");
         System.out.println("2. FizzBuzz program");
         System.out.println("Option: ");
+        // Get user's choice
         Scanner scanner = new Scanner(System.in);
         int optionSelected = Integer.parseInt(scanner.nextLine());
+        // Based on user's choice, we call the correct class
         if (optionSelected == 1) FindNumber.play();
         else if(optionSelected == 2) FizzBuzz.play();
     }
